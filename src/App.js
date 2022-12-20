b5b88c6a4989fe3fa8c5bd352001911a296cb08c
yyy-muu <rirakkuma.0918.y@gmail.com>
2022-12-20 21:59:25 +0900

    Add react routing settings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import { Home } from "./components/Home";
+import { CreatePost } from "./components/CreatePost";
+import { Login } from "./components/Login";
+import { Logout } from "./components/Logout";
 
 function App() {
-  return <div></div>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/createpost" element={<CreatePost />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
